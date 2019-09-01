04c61e2e57c69ed5a26462bce4770c1ad962a11c
sophieyoung <sophieyoung@152.23.77.85>
2019-09-01 16:33:16 -0400

    first loop

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -9,6 +9,28 @@ public class A1Adept {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
+		int items = scan.nextInt();
 		
+		// put item names and prices into arrays
+		String[] itemTypes = new String [items];
+		double[] itemPrices = new double [items];
+		
+		for (int i = 0; i < items; i++) {
+			String name = scan.next();
+			int price = scan.nextInt();
+		}
+		
+		
+		
+		
+		int totalCustomers = scan.nextInt();
+		// make for loop, get first and last name, first name array, last name array
+		String[] nameOfCustomers = new String[totalCustomers];
+		double[] totalSpent = new double[totalCustomers];
+		
+		for (int j = 0; j < totalCustomers; j++) {
+			nameOfCustomers[j] = scan.next() + " " + scan.next();
+		}
+		System.out.println("Biggest:" + "Smallest:" + "Average:")
 	}
 }
