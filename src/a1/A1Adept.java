5d669a2800664dad0d209fd01dde778e9aef93b2
sophieyoung <sophieyoung@152.23.77.85>
2019-09-01 17:33:55 -0400

    completed code with bugs

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -8,6 +8,7 @@ public class A1Adept {
 		
 		Scanner scan = new Scanner(System.in);
 
+		
 		// Your code follows here.
 		int items = scan.nextInt();
 		
@@ -16,8 +17,8 @@ public class A1Adept {
 		double[] itemPrices = new double [items];
 		
 		for (int i = 0; i < items; i++) {
-			String name = scan.next();
-			int price = scan.nextInt();
+			itemTypes[i] = scan.next();
+			itemPrices[i] = scan.nextInt();
 		}
 		
 		
@@ -27,10 +28,54 @@ public class A1Adept {
 		// make for loop, get first and last name, first name array, last name array
 		String[] nameOfCustomers = new String[totalCustomers];
 		double[] totalSpent = new double[totalCustomers];
+		int allExpenses = 0;
 		
 		for (int j = 0; j < totalCustomers; j++) {
 			nameOfCustomers[j] = scan.next() + " " + scan.next();
+			int itemAmount = scan.nextInt();
+			
+			for (int k = 0; k < itemAmount; k++) { 
+				int singleItemQuantity = scan.nextInt();
+				String singleItemName = scan.next();
+				
+				double cost = 0.0;
+				
+				for (int l = 0; l < items; l++) {
+					if (singleItemName.equals(itemTypes[l])) {
+						cost = itemPrices[l] * singleItemQuantity; 
+					}
+					totalSpent[j] += cost;
+				}
+				
+			}
+			allExpenses += totalSpent[j];
 		}
-		System.out.println("Biggest:" + "Smallest:" + "Average:")
+		int largestIndex = 0;
+		int smallestIndex = 0;
+			
+		for (int i = 0; i < totalCustomers; i++) {
+			if (totalSpent[i] > totalSpent[largestIndex]) {
+				largestIndex = i;
+			}		
+			if (totalSpent[i] < totalSpent[smallestIndex]) {
+				smallestIndex = i;
+		}
+
+		}
+		scan.close();
+		int valueSum = 0;		
+		for (int i=0; i<totalCustomers; i++) {
+				valueSum += totalSpent[i];
+			}
+		}
+	
+	// names at biggest, customer totals at biggest
+		System.out.println("Biggest:" + nameOfCustomers[largestIndex] + String.format("%.2f", largestIndex)) 
+		System.out.println("Smallest:" + nameOfCustomers[smallestIndex] + String.format("%.2f", smallestIndex))
+		System.out.println("Average:" + String.format("%.2f", (valueSum / totalCustomers)));
 	}
-}
+
+	
+	
+	
+	
